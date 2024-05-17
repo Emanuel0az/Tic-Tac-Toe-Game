c769f8c3fbbfb3113c31a94a8d525139e2e60085
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-05-17 08:10:30 -0600

    correccion

---
+++ index.js
@@ -0,0 +1,13 @@
+const matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log(matriz);
+
+let gato = document.getElementsByClassName('cell')
+
+function letra() {
+    console.log("funciona");
+}
