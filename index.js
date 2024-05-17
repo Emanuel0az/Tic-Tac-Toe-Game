857cd1b0e5e2b864eda7179a31214757780ae597
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-05-17 14:33:57 -0600

    avance de la funcion del juego

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ let boton6 = document.getElementById ('boton6')
 let boton7 = document.getElementById ('boton7')
 let boton8 = document.getElementById ('boton8')
 let boton9 = document.getElementById ('boton9')
+let text_0 = "O"
+let text_x = "X"
+let jugador = text_0
 
 
 const matriz = [
@@ -32,55 +35,10 @@ console.log(matriz);
 
 let validar1 = true
 
-boton1.addEventListener('click',function(){
-    if(validar1 === true){
-        if(boton1 == ''){
-            boton1.innerHTML = "X"
-        }
-        else{
-            
-        }
-    
-    }
-    console.log("boton1");
-})
-
-boton2.addEventListener('click',function(){
-    boton2.innerHTML = "X"
-    console.log("boton2");
-})
-
-boton3.addEventListener('click',function(){
-    boton3.innerHTML = "X"
-    console.log("boton3");
-})
-
-boton4.addEventListener('click',function(){
-    boton4.innerHTML = "X"
-    console.log("boton4");
-})
-
-boton5.addEventListener('click',function(){
-    boton5.innerHTML = "X"
-    console.log("boton5");
-})
-
-boton6.addEventListener('click',function(){
-    boton6.innerHTML = "X"
-    console.log("boton6");
-})
-
-boton7.addEventListener('click',function(){
-    boton7.innerHTML = "X"
-    console.log("boton7");
-})
-
-boton8.addEventListener('click',function(){
-    boton8.innerHTML = "X"
-    console.log("boton8");
-})
-
-boton9.addEventListener('click',function(){
-    boton9.innerHTML = "X"
-    console.log("boton9");
-})
+for (let index = 0; index < gato.length; index++) {
+    gato[index].addEventListener('click',function(){
+        gato[index].innerHTML= "X"
+       gato[3].innerHTML = "O"
+    })
+        
+}   
