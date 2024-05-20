798e3abff0c5311e475a7271085c6316a6bc9973
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-05-20 10:13:03 -0600

    contador de delay para la maquina

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const matriz = [
     [boton4, boton5, boton6],
     [boton7, boton8, boton9]
 ]
-console.log(matriz);// retornamos matriz
+
 
 
 
@@ -32,12 +32,8 @@ console.log(matriz);// retornamos matriz
 function aleatorio() {
 
     let espacio = [];// encuentra las casillas vacias del array
-
-
     // itera sobre las cassillas del array
     for (let index = 0; index < gato.length; index++) {
-
-
         // verifica si cada cassilla esta vacia
         if (gato[index].innerHTML === "") {
             espacio.push(index);
@@ -52,25 +48,36 @@ function aleatorio() {
     }
     return null; //busca una casilla vacia en el i y si no hay devuelbe nulo
 }
-
-
-
+        
 // itera sobre las casillas del array
 for (let index = 0; index < gato.length; index++) {
-
-    // le da un evento de clic a cada una de las casillas
-    gato[index].addEventListener('click', function() {
-
-        // verifica si la casilla esta vacia para marcar
-        if (gato[index].innerHTML === "") {
-            gato[index].innerHTML = "X"; // agrega una "X" a la casilla vacia
+        // le da un evento de clic a cada una de las casillas
+        gato[index].addEventListener('click', function() {
+                // verifica si la casilla esta vacia para marcar
+                if (gato[index].innerHTML === "") {
+                    gato[index].innerHTML = "X"; // agrega una "X" a la casilla vacia
 
 
-            let maquina = aleatorio(); // busca un espacio vacio para la maquina
-            if (maquina !== null) {
-                gato[maquina].innerHTML = "O"; // si la casilla esta vacia marca con una "O"
+                    let maquina = aleatorio(); // busca un espacio vacio para la maquina
+                    setTimeout(function() {
+                        if (maquina !== null) {
+                            gato[maquina].innerHTML = "O"; // si la casilla esta vacia marca con una "O"
+            
+                        }
+                    }, 1000);
+                }
+                
+                
+        })
 
-            }
-        }
-    });
+} 
+
+
+function validarFilas() {
+    for (let j = 0; j < array.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+            
+        }        
+    }
 }
+
